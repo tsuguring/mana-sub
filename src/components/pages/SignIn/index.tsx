b1dec48f8cff0676d841b84e7b5c163309963b23
tsuguri <tsuguri4429@gmail.com>
2021-10-08 22:38:45 +0900

    create signin-page

--- src/components/pages/SignIn/index.tsx
+++ src/components/pages/SignIn/index.tsx
@@ -1,18 +1,67 @@
 import React from 'react';
-import { View, StyleSheet, Text } from 'react-native';
+import { View, StyleSheet, TouchableWithoutFeedback } from 'react-native';
+import { useControlledComponent } from '../../lib/hooks';
+import { Button, dismiss, TextField } from '../../atoms';
+import SignInWithGoogle from './SignInWithGoogle';
+import { Context, Status } from '../../../contexts/ui';
+import { COLOR } from '../../../constants/theme';
 
 const styles = StyleSheet.create({
     container: {
         flex: 1,
+        padding: 16,
         justifyContent: 'center',
-        alignItems: 'center',
-    }
+        backgroundColor: COLOR.MAIN
+    },
+    text: {
+        marginVertical: 20,
+    },
+    button: {
+        marginTop: 50,
+    },
+    textContainer: {
+        flex: 1,
+        justifyContent: 'center',
+    },
+    buttonContainer: {
+        flex: 1,
+        justifyContent: 'flex-start',
+    },
 });
 
 export default function SignIn() {
+    const { setApplicationState } = React.useContext(Context);
+    const mailAddress = useControlledComponent('');
+    const password = useControlledComponent('');
+
     return (
-        <View style={styles.container}>
-            <Text>SignIn</Text>
-        </View>
-    )
+        <TouchableWithoutFeedback onPress={dismiss}>
+            <View style={styles.container}>
+                <View style={styles.textContainer}>
+                    <TextField
+                        label="email"
+                        value={mailAddress.value}
+                        onChangeText={mailAddress.onChangeText}
+                        style={styles.text}
+                        autoCompleteType="email"
+                    />
+                    <TextField
+                        label="password"
+                        value={password.value}
+                        onChangeText={password.onChangeText}
+                        style={styles.text}
+                        autoCompleteType="password"
+                    />
+                </View>
+                <View style={styles.buttonContainer}>
+                    <SignInWithGoogle/>
+                    <Button 
+                        onPress={() => setApplicationState(Status.AUTHORIZED)}
+                        style={styles.button}
+                        label="登録"
+                    />
+                </View>
+            </View>
+        </TouchableWithoutFeedback>
+    );
 }
