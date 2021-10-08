82530057b6ee434d9c3949e13ba6e8607928b864
tsuguri <tsuguri4429@gmail.com>
2021-10-08 21:35:16 +0900

    add atomic design

---
+++ src/components/atoms/TextField.tsx
@@ -0,0 +1,68 @@
+import React from "react";
+import { ViewStyle, Keyboard } from 'react-native';
+import { TextInput } from 'react-native-paper';
+import { COLOR } from '../../constants/theme';
+
+interface Props {
+    label: string;
+    value: string;
+    onChangeText?: (str: string) => void;
+    style?: ViewStyle;
+    autoCompleteType?:
+        | 'off'
+        | 'username'
+        | 'password'
+        | 'email'
+        | 'name'
+        | 'tel'
+        | 'street-address'
+        | 'postal-code'
+        | 'cc-number'
+        | 'cc-csc'
+        | 'cc-exp'
+        | 'cc-exp-month'
+        | 'cc-exp-year'
+    secureTextEntry?: boolean;
+    disabled?: boolean;
+}
+
+const theme = {
+    dark: true,
+    colors: {
+        primary: COLOR.PRIMARY,
+        background: COLOR.MAIN,
+        text: COLOR.WHITE,
+        placeholder: COLOR.PRIMARY,
+    },
+};
+
+export default function TextField(props: Props) {
+    const {
+        label,
+        value,
+        onChangeText = () => {},
+        style,
+        autoCompleteType,
+        secureTextEntry,
+        disabled
+    } = props;
+
+    return (
+        <TextInput
+            label={label}
+            value={value}
+            disabled={disabled}
+            onChangeText={onChangeText}
+            mode="outlined"
+            theme={theme}
+            style={style}
+            autoCompleteType={autoCompleteType}
+            autoCapitalize="none"
+            secureTextEntry={secureTextEntry}
+        />
+    );
+}
+
+export function dismiss() {
+    Keyboard.dismiss();
+}
--- src/components/pages/SignUp/index.tsx
+++ src/components/pages/SignUp/index.tsx
@@ -1,18 +1,52 @@
 import React from 'react';
-import { View, StyleSheet, Text } from 'react-native';
+import { View, StyleSheet, TouchableWithoutFeedback} from 'react-native';
+import { Button, dismiss, TextField } from '../../atoms';
+import{ Context, Status } from '../../../contexts/ui';
+import { useControlledComponent } from '../../lib/hooks';
+import { COLOR } from '../../../constants/theme';
 
 const styles = StyleSheet.create({
     container: {
         flex: 1,
-        justifyContent: 'center',
-        alignItems: 'center',
-    }
+        padding: 16,
+        backgroundColor: COLOR.MAIN,
+    },
+    text: {
+        marginVertical: 20,
+    },
+    button: {
+        marginTop: 20,
+    },
 });
 
 export default function SignUp() {
+    const { setApplicationState } = React.useContext(Context);
+    const mailAddress = useControlledComponent('');
+    const password = useControlledComponent('');
+
     return (
-        <View style={styles.container}>
-            <Text>SignUp</Text>
-        </View>
-    )
+        <TouchableWithoutFeedback onPress={dismiss}>
+            <View style={styles.container}>
+                <TextField
+                    label="email"
+                    value={mailAddress.value}
+                    onChangeText={mailAddress.onChangeText}
+                    style={styles.text}
+                    autoCompleteType="email"
+                />
+                <TextField
+                    label="password"
+                    value={password.value}
+                    onChangeText={password.onChangeText}
+                    style={styles.text}
+                    autoCompleteType="password"
+                />
+                <Button 
+                    onPress={() => setApplicationState(Status.AUTHORIZED)}
+                    style={styles.button}
+                    label="登録"
+                />
+            </View>
+        </TouchableWithoutFeedback>
+    );
 }
