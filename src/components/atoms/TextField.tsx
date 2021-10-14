182f4c4b1299777ffce3f0b7bcd6bae28bdd1b43
tsuguri <tsuguri4429@gmail.com>
2021-10-14 23:48:59 +0900

    change layout

--- src/components/atoms/TextField.tsx
+++ src/components/atoms/TextField.tsx
@@ -31,7 +31,7 @@ const theme = {
     colors: {
         primary: COLOR.PRIMARY,
         background: COLOR.WHITE,
-        text: COLOR.WHITE,
+        text: COLOR.BLACK,
         placeholder: COLOR.PRIMARY,
     },
 };
