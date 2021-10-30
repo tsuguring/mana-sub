4b63a430b97b785abcac7dd5e28fffcaa6341c4d
tsuguri <tsuguri4429@gmail.com>
2021-10-31 00:49:08 +0900

    demo sumsubscribe

--- src/components/molecules/Sumsubscribe.tsx
+++ src/components/molecules/Sumsubscribe.tsx
@@ -1,20 +1,41 @@
 import React from "react";
-import { Text, TouchableHighlight, View, StyleSheet } from "react-native";
+import { Text, View, Button, Alert, StyleSheet } from "react-native";
 import { COLOR } from "../../constants/theme";
+
+const styles = StyleSheet.create({
+  container: {
+    backgroundColor: COLOR.MAIN_LIGHT,
+    paddingTop: 20,
+    paddingBottom: 20,
+    paddingLeft: 50,
+    paddingRight: 50,
+    marginTop: 30,
+    marginBottom: 30,
+  },
+  text: {
+    fontSize: 32,
+  },
+  button: {
+    backgroundColor: COLOR.WHITE,
+  },
+});
 
 interface Props {
   id: string;
   title: string;
   money: string;
   period: string;
-  date: Date;
   detail?: string;
 }
 
 export default function Sumsubscribe() {
   return (
-    <View>
-      <Text>あいうえお</Text>
+    <View style={styles.container}>
+      <Text style={styles.text}>¥5,400</Text>
+      <Button
+        onPress={() => Alert.alert("まだ実装していません")}
+        title="並び替え"
+      />
     </View>
   );
 }
--- src/components/pages/Home/index.tsx
+++ src/components/pages/Home/index.tsx
@@ -12,6 +12,7 @@ const styles = StyleSheet.create({
     flex: 1,
     justifyContent: "center",
     alignItems: "center",
+    backgroundColor: COLOR.MAIN_DARK,
   },
   button: {
     position: "absolute",
