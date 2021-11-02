d3f4ada542bf1ae348f6aa7304df122bcf086505
tsuguri <tsuguri4429@gmail.com>
2021-11-02 13:50:47 +0900

    install firebase

--- App.tsx
+++ App.tsx
@@ -1,12 +1,31 @@
-import React from 'react';
-import * as UiContext from './src/contexts/ui'
-import { StyleSheet, Text, View } from 'react-native';
-import Routes from './src/routes';
+import React from "react";
+import * as UiContext from "./src/contexts/ui";
+import { StyleSheet, Text, View } from "react-native";
+import Routes from "./src/routes";
+import firebase from "firebase";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBkKvlpH5GlYUOSCHJ5suyzJUC3FlJT6bU",
+  authDomain: "mana-sub.firebaseapp.com",
+  projectId: "mana-sub",
+  storageBucket: "mana-sub.appspot.com",
+  messagingSenderId: "1051846455159",
+  appId: "1:1051846455159:web:a7cd151cb249080e17fb72",
+  measurementId: "G-7LC0CNB2E7",
+};
+
+if (firebase.app.length === 0) {
+  firebase.initializeApp(firebaseConfig);
+}
 
 export default function App() {
-  const [applicationState, setApplicationState] = React.useState(UiContext.createApplicationInitialState());
+  const [applicationState, setApplicationState] = React.useState(
+    UiContext.createApplicationInitialState()
+  );
   return (
-    <UiContext.Context.Provider value={{ applicationState, setApplicationState }}>
+    <UiContext.Context.Provider
+      value={{ applicationState, setApplicationState }}
+    >
       <Routes />
     </UiContext.Context.Provider>
   );
@@ -15,8 +34,8 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
