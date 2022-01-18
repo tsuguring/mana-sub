2d5510a3087a929937a14f2a1b3164a90dea4ba4
tsuguri <tsuguri4429@gmail.com>
2022-01-18 17:01:17 +0900

    fix carousel

--- src/components/molecules/CarouselItem.tsx
+++ src/components/molecules/CarouselItem.tsx
@@ -1,39 +1,20 @@
 import React from "react";
-import { StyleSheet, Text, View } from "react-native";
+import { StyleSheet, Text, View, Image } from "react-native";
 import Button from "../atoms/Button";
 import { COLOR } from "../../constants/theme";
-import { width } from "../../lib/window";
 
-const padding = 20;
-const edgeNumber = 2;
+const padding = 40;
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: "center",
     alignItems: "center",
+    justifyContent: "center",
     padding,
     backgroundColor: COLOR.MAIN,
   },
   text: {
-    fontSize: 18,
-    fontWeight: "500",
-    lineHeight: 40,
-  },
-  textContainer: {
-    flex: 1,
-    justifyContent: "flex-start",
-    alignItems: "center",
-    width: width - padding * edgeNumber,
-    paddingVertical: 10,
-  },
-  imageContainer: {
-    flex: 2,
-  },
-  contentContainer: {
-    flex: 3,
-    paddingTop: 30,
-    justifyContent: "space-between",
-    paddingBottom: 20,
+    lineHeight: 30,
+    marginBottom: 40,
   },
 });
 
@@ -48,18 +29,9 @@ export default function CarouselItem({
 }) {
   return (
     <View style={styles.container}>
-      <View style={styles.textContainer}>
-        <View style={styles.contentContainer}>
-          <View>
-            <Text style={styles.text}>{item.text}</Text>
-          </View>
-          {index === 2 ? (
-            <Button onPress={onPress} label="始める!" />
-          ) : (
-            <Text>スワイプ</Text>
-          )}
-        </View>
-      </View>
+      <Image source={require("../../../assets/carousel_image1.png")} />
+      <Text style={styles.text}>{item.text}</Text>
+      {index === 2 ? <Button onPress={onPress} label="始める!" /> : <></>}
     </View>
   );
 }
