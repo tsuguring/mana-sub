74b16cc6fad1a66e28a6eb8bf1e3e3936f66eec6
tsuguri <tsuguri4429@gmail.com>
2022-01-27 22:33:20 +0900

    create setting page

--- src/components/pages/Setting/index.tsx
+++ src/components/pages/Setting/index.tsx
@@ -1,18 +1,121 @@
-import React from 'react';
-import { View, StyleSheet, Text } from 'react-native';
+import React from "react";
+import { View, StyleSheet, Text } from "react-native";
+import { LinearGradient } from "expo-linear-gradient";
+import { COLOR } from "../../../constants/theme";
+import { TouchableOpacity } from "react-native-gesture-handler";
 
 const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        justifyContent: 'center',
-        alignItems: 'center',
-    }
+  container: {
+    flex: 1,
+  },
 });
 
 export default function Setting() {
-    return (
-        <View style={styles.container}>
-            <Text>Setting</Text>
-        </View>
-    )
+  return (
+    <LinearGradient
+      colors={[COLOR.MAIN, COLOR.MAIN, COLOR.WHITE]}
+      style={styles.container}
+    >
+      <View>
+        <Text
+          style={{
+            padding: 14,
+            fontSize: 16,
+          }}
+        >
+          設定
+        </Text>
+        <TouchableOpacity
+          style={{
+            borderWidth: 1,
+            borderColor: "#ccc",
+            padding: 14,
+            backgroundColor: COLOR.WHITE,
+          }}
+        >
+          <Text style={{ fontSize: 16 }}>支払日前に通知でお知らせ</Text>
+        </TouchableOpacity>
+        <TouchableOpacity
+          style={{
+            borderWidth: 1,
+            borderColor: "#ccc",
+            padding: 14,
+            backgroundColor: COLOR.WHITE,
+          }}
+        >
+          <Text style={{ fontSize: 16 }}>支払日前に通知でお知らせ</Text>
+        </TouchableOpacity>
+        <TouchableOpacity
+          style={{
+            borderWidth: 1,
+            borderColor: "#ccc",
+            padding: 14,
+            backgroundColor: COLOR.WHITE,
+          }}
+        >
+          <Text style={{ fontSize: 16 }}>支払日前に通知でお知らせ</Text>
+        </TouchableOpacity>
+        <TouchableOpacity
+          style={{
+            borderWidth: 1,
+            borderColor: "#ccc",
+            padding: 14,
+            backgroundColor: COLOR.WHITE,
+          }}
+        >
+          <Text style={{ fontSize: 16 }}>支払日前に通知でお知らせ</Text>
+        </TouchableOpacity>
+      </View>
+      <View>
+        <Text
+          style={{
+            padding: 14,
+            fontSize: 16,
+          }}
+        >
+          設定
+        </Text>
+        <TouchableOpacity
+          style={{
+            borderWidth: 1,
+            borderColor: "#ccc",
+            padding: 14,
+            backgroundColor: COLOR.WHITE,
+          }}
+        >
+          <Text style={{ fontSize: 16 }}>支払日前に通知でお知らせ</Text>
+        </TouchableOpacity>
+        <TouchableOpacity
+          style={{
+            borderWidth: 1,
+            borderColor: "#ccc",
+            padding: 14,
+            backgroundColor: COLOR.WHITE,
+          }}
+        >
+          <Text style={{ fontSize: 16 }}>支払日前に通知でお知らせ</Text>
+        </TouchableOpacity>
+        <TouchableOpacity
+          style={{
+            borderWidth: 1,
+            borderColor: "#ccc",
+            padding: 14,
+            backgroundColor: COLOR.WHITE,
+          }}
+        >
+          <Text style={{ fontSize: 16 }}>支払日前に通知でお知らせ</Text>
+        </TouchableOpacity>
+        <TouchableOpacity
+          style={{
+            borderWidth: 1,
+            borderColor: "#ccc",
+            padding: 14,
+            backgroundColor: COLOR.WHITE,
+          }}
+        >
+          <Text style={{ fontSize: 16 }}>支払日前に通知でお知らせ</Text>
+        </TouchableOpacity>
+      </View>
+    </LinearGradient>
+  );
 }
