ffc5b3959be0775ba448bab7669a7c004be7b9b7
tsuguri <tsuguri4429@gmail.com>
2022-02-06 17:21:35 +0900

    create privacy policy page

---
+++ src/components/pages/Privacy/index.tsx
@@ -0,0 +1,29 @@
+import React from "react";
+import { StyleSheet, Text } from "react-native";
+import { LinearGradient } from "expo-linear-gradient";
+import { COLOR } from "../../../constants/theme";
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  itemcontainer: {
+    borderWidth: 1,
+    borderColor: "#ccc",
+    padding: 14,
+    backgroundColor: COLOR.WHITE,
+  },
+});
+
+export default function Inquiries() {
+  return (
+    <LinearGradient
+      colors={[COLOR.MAIN, COLOR.MAIN, COLOR.WHITE]}
+      style={styles.container}
+    >
+      <Text>Privacy</Text>
+    </LinearGradient>
+  );
+}
