3deb340001ca3b57e68d4a482d7f6203a8ddf35c
tsuguri <tsuguri4429@gmail.com>
2022-02-19 17:38:36 +0900

    firebase-analytics

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,10 +1,36 @@
-import React from "react";
-import { NavigationContainer } from "@react-navigation/native";
+import React, { useRef } from "react";
+import {
+  NavigationContainer,
+  useNavigationContainerRef,
+} from "@react-navigation/native";
 import MainRoutes from "./Main";
+import * as Analytics from "expo-firebase-analytics";
 
 export default function LoggingRoutes() {
+  const navigationRef = useNavigationContainerRef();
+  const routeNameRef = useRef<string>();
+
   return (
-    <NavigationContainer>
+    <NavigationContainer
+      ref={navigationRef}
+      onReady={() => {
+        routeNameRef.current = navigationRef?.current?.getCurrentRoute()?.name;
+      }}
+      onStateChange={async () => {
+        const previousRouteName = routeNameRef.current;
+        const currentRouteName = navigationRef?.getCurrentRoute()?.name;
+
+        if (previousRouteName !== currentRouteName) {
+          // The line below uses the expo-firebase-analytics tracker
+          // https://docs.expo.io/versions/latest/sdk/firebase-analytics/
+          // Change this line to use another Mobile analytics SDK
+          await Analytics.setCurrentScreen(currentRouteName);
+        }
+
+        // Save the current route name for later comparison
+        routeNameRef.current = currentRouteName;
+      }}
+    >
       <MainRoutes />
     </NavigationContainer>
   );
