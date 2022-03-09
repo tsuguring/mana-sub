ea07c1e5a9782ab6191175b7bc870bfef97f0d2a
tsuguri <tsuguri4429@gmail.com>
2022-03-09 14:21:14 +0900

    delete files

--- src/components/atoms/index.ts
+++ src/components/atoms/index.ts
@@ -1,6 +1,4 @@
 export { default as Button } from "./Button";
 export { default as Pagination } from "./Pagination";
 export { default as Admob } from "./Admob";
-export { default as AddNotification } from "./AddNotification";
 export { default as CanselNotification } from "./CanselNotification";
-export { default as FixNotification } from "./FixNotification";
